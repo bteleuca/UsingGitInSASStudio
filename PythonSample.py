a2dc64930109df50b7469d0cf5aac778d795ad54
bteleuca <bogdan.teleuca@gmail.com>
2024-11-01 07:31:28 +0000

    Changed program to convert SAS dates to DATETIME

--- PythonSample.py
+++ PythonSample.py
@@ -1,21 +1,12 @@
-import os, sys
-sys.path
+import numpy as np
+import pandas as pd
 
-# define our data references
-input_table = 'SASHELP.CLASS'
-output_table = 'WORK.PYTHONOUT'
+def py_func (sasdate:float):
+	# complicated python logic
+	ser = pd.Series([sasdate])
+	ser = pd.to_timedelta(ser, unit='D') + pd.Timestamp('1960-1-1')
+	dttm=ser[0]
+	return dttm
 
-# set input data to pipe from SAS
-dfin = SAS.sd2df(input_table)
-
-# output to the log details about the table
-print("input data shape is:", dfin.shape)
-
-# call the python transpose
-dfout = dfin.transpose()
-
-# output to the log details about the table
-print("output data shape is:", dfout.shape)
-
-# set output data using call to pipe to SAS
-SAS.df2sd(dfout, output_table)
+sasdate = 19778
+print('The SAS date ', sasdate, 'resolves to: ', py_func(19778.0))
