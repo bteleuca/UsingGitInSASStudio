e7d6d23910f253f2161b4bac5df661d1d973f2d8
bteleuca <bogdan.teleuca@sas.com>
2024-12-08 20:25:52 -0500

    SAS Workbench development

--- PythonSample.py
+++ PythonSample.py
@@ -19,3 +19,14 @@ print("output data shape is:", dfout.shape)
 
 # set output data using call to pipe to SAS
 SAS.df2sd(dfout, output_table)
+import numpy as np
+import pandas as pd
+
+def py_func(sasdate: float):
+    ser = pd.Series([sasdate])
+    ser = pd.to_timedelta(ser, unit='D') + pd.Timestamp('1960-1-1')
+    dttm = ser[0]
+    return dttm
+
+sasdate = 19778
+print('The SAS date ', sasdate, 'resolves to: ', py_func(19778.0))
